8f0e97e4cc86d6a28c6fb47e5981b383dd308cfe
karthik0146 <byreddykartheek@gmail.com>
2026-06-03 05:17:20 +0530

    added pwa

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,47 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-   server: {
-    host: '0.0.0.0',
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "CoreBugs",
+        short_name: "CoreBugs",
+
+        description: "Verilog Learning & Recruitment Platform",
+
+        theme_color: "#0f172a",
+        background_color: "#0f172a",
+
+        display: "standalone",
+
+        start_url: "/",
+
+        icons: [
+          {
+    src: "/web-app-manifest-192x192.png",
+    sizes: "192x192",
+    type: "image/png",
+  },
+  {
+    src: "/web-app-manifest-512x512.png",
+    sizes: "512x512",
+    type: "image/png",
+  },
+        ],
+      },
+    }),
+  ],
+
+  server: {
+    host: "0.0.0.0",
     port: 5173,
-    strictPort: true
-  }
-})
+    strictPort: true,
+  },
+});
